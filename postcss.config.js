2e3f54eb0da8a5575dd3fb42fbf8703a924b86a3
Noskov Artem <nowasmawesome@gmail.com>
2019-08-10 16:21:42 +0500

    Changed logo, Added my styles and dark theme

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,19 @@
 let plugins = [
-  require("postcss-import"),
-  require("autoprefixer")({ browsers: "last 2 versions" })
+    require("postcss-import"),
+    require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require("postcss-nesting"),
+    require("autoprefixer")({ overrideBrowserslist: "> 1%, last 2 versions" })
 ];
 
 // if (process.env.NODE_ENV == 'production') plugins.push(require("postcss-csso"))
 if (process.env.NODE_ENV == "production")
-  plugins.push(
-    require("cssnano")({
-      preset: "default"
-    })
-  );
+    plugins.push(
+        require("cssnano")({
+            preset: "default"
+        })
+    );
 
 module.exports = {
-  plugins
+    plugins
 };
